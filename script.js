b01febe6cd7f50a33e84bde88a29a6570ce7d82e
jovasal <jovasal@gmail.com>
2023-08-27 22:55:37 -0500

    popup

--- script.js
+++ script.js
@@ -22,3 +22,26 @@ function toggleMenu() {
       toggleMenu();
     });
   });
+
+  const popupButton1 = document.getElementById('popupbutton1');
+    const popupButton2 = document.getElementById('popupbutton2');
+    const popup1 = document.getElementById('popup1');
+    const popup2 = document.getElementById('popup2');
+    const close1 = document.getElementById('close1');
+    const close2 = document.getElementById('close2');
+
+    popupButton1.addEventListener('click', () => {
+        popup1.style.display = 'block';
+    });
+
+    popupButton2.addEventListener('click', () => {
+        popup2.style.display = 'block';
+    });
+
+    close1.addEventListener('click', () => {
+        popup1.style.display = 'none';
+    });
+
+    close2.addEventListener('click', () => {
+        popup2.style.display = 'none';
+    });
